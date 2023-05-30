6f92a5678c2a5d39d6358c774235a24388d1dc67
LM <hi@leima.is>
2023-05-30 22:20:48 +0200

    more charts

--- code/update_data.py
+++ code/update_data.py
@@ -12,11 +12,11 @@ speadsheet_urls = {
         "https://docs.google.com/spreadsheets/d/e/"
         "2PACX-1vRe7-Yk7TxigT4MK3qz-RbuVt7lZu0u4vQjiqotiXCT1Ynb9sUrO7tA1RdPemXKaE2dD8IYYO5K0E1C"
         "/pub?gid=1162044269&single=true&output=csv"
-    )
+    ),
 }
 
-df_targets = pd.read_csv(speadsheet_urls["targets"]).dropna(how="all",axis=0)
-df_sensors = pd.read_csv(speadsheet_urls["sensors"]).dropna(how="all",axis=0)
+df_targets = pd.read_csv(speadsheet_urls["targets"]).dropna(how="all", axis=0)
+df_sensors = pd.read_csv(speadsheet_urls["sensors"]).dropna(how="all", axis=0)
 
 df_sensors.to_csv("dataset/sensor_data.csv", index=False)
 df_targets.to_csv("dataset/target_data.csv", index=False)
--- code/update_visualization.py
+++ code/update_visualization.py
@@ -18,9 +18,11 @@ fig_box_targets = px.box(
     color="Title",
 )
 
-fig_box_targets.write_json(
-    file=charts_base_path / "visualization_box_targets.json"
+fig_box_targets.update_layout(
+    legend=dict(orientation="h", yanchor="bottom", y=-0.4, xanchor="right", x=1)
 )
+
+fig_box_targets.write_json(file=charts_base_path / "visualization_box_targets.json")
 
 # %%
 # sensor data
@@ -30,9 +32,11 @@ fig_box_sensor = px.box(
     color="Title",
 )
 
-fig_box_sensor.write_json(
-    file=charts_base_path / "visualization_box_sensors.json"
+fig_box_sensor.update_layout(
+    legend=dict(orientation="h", yanchor="bottom", y=-0.4, xanchor="right", x=1)
 )
+
+fig_box_sensor.write_json(file=charts_base_path / "visualization_box_sensors.json")
 
 # %%
 def cart2pol(x, y):
@@ -40,37 +44,26 @@ def cart2pol(x, y):
     rho = np.hypot(x, y)
     return theta, rho
 
-df_targets[["theta", "rho"]] = list(df_targets.apply(lambda x: cart2pol(x.x, x.y), axis=1))
+
+df_targets[["theta", "rho"]] = list(
+    df_targets.apply(lambda x: cart2pol(x.x, x.y), axis=1)
+)
 
 fig_polar = px.scatter_polar(
     df_targets,
-    r="rho", theta="theta",
+    r="rho",
+    theta="theta",
     color="Title",
-    color_discrete_sequence=px.colors.sequential.Plasma_r
+    color_discrete_sequence=px.colors.sequential.Plasma_r,
 )
 
 fig_polar.update_layout(
-    polar = dict(
-        radialaxis = dict(
-            tickvals=[],
-            ticktext=[]
-        ),
-        angularaxis = dict(
-            tickmode="array",
-            tickvals=[],
-            ticktext=[]
-            )
+    polar=dict(
+        radialaxis=dict(tickvals=[], ticktext=[]),
+        angularaxis=dict(tickmode="array", tickvals=[], ticktext=[]),
     ),
-    legend=dict(
-    orientation="h",
-    yanchor="bottom",
-    y=-0.4,
-    xanchor="right",
-    x=1
-)
+    legend=dict(orientation="h", yanchor="bottom", y=-0.4, xanchor="right", x=1),
 )
 
-fig_polar.write_json(
-    file=charts_base_path / "visualization_polar_targets.json"
-)
+fig_polar.write_json(file=charts_base_path / "visualization_polar_targets.json")
 # %%
